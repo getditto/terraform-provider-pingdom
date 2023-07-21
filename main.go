e35b05d8b67972e9dd127ada6768ce2555de709c
Moustafa Baiou <moustafa@ditto.live>
2023-07-21 15:56:43 -0400

    fix: make actions work

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"github.com/getditto/terraform-provider-pingdom/pingdom"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/nordcloud/terraform-provider-pingdom/pingdom"
 )
 
 func main() {
